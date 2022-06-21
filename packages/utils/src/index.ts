b0230fadf70f6d64e9cca3954097dc6c7ddbfaae
lffloyd <lfmatosmelo@id.uff.br>
2022-06-20 21:50:16 -0300

    chore(utils): adds docs to ResponseCreator's send method

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -8,7 +8,12 @@ export type HttpResponse =  {
 the client
 */
 export class ResponseCreator {
-
+    /**
+     * Sends a HTTP response to the consumer.
+     * @param status HTTP status code to return
+     * @param message message to the consumer. Can be any primitive type or object
+     * @returns HTTP response
+     */
     static send(status: number, message?: object): HttpResponse {
         return {
             statusCode: status,
