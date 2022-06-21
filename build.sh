55b7d39fc04e2134cf6a2267e1aaef67c41a4828
lffloyd <lfmatosmelo@id.uff.br>
2022-06-20 22:42:48 -0300

    feat(scripts): updates build scripts for ci

--- build.sh
+++ build.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+just_affected=$1
+
 npm install || exit 1
 npm run clean || exit 1
 npm run bootstrap || exit 1
-npm run build || exit 1
-npm test || exit 1
+if [[ $just_affected == "true" ]]; then
+    npm run buildAffected || exit 1
+    npm run testAffected || exit 1
+    npm run publishCi || exit 1
+else
+    npm run build || exit 1
+    npm test || exit 1
+fi
+
