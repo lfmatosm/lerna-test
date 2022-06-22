c782fd23910bedf32ed4ab13bfbc15754a7bd12c
lffloyd <lfmatosmelo@id.uff.br>
2022-06-21 23:16:17 -0300

    chore(scripts):

--- affected.sh
+++ affected.sh
@@ -2,4 +2,7 @@
 
 changed_packages=$(npx lerna changed)
 scope=$(node -e "console.log('{'+process.argv.slice(1).join(',')+'}')" $changed_packages)
+if [[ $scope == "{}" ]]; then
+    echo "No packages affected!" && exit 0
+fi
 echo $scope
