bf5365806e6f60bc01ca2bf08e1952dedbe159d6
lffloyd <lfmatosmelo@id.uff.br>
2022-06-22 23:13:12 -0300

    chore(scripts): updates script to find affected or changed packages

--- affected.sh
+++ affected.sh
@@ -2,7 +2,10 @@
 
 changed_packages=$(npx lerna changed)
 scope=$(node -e "console.log('{'+process.argv.slice(1).join(',')+'}')" $changed_packages)
+
 if [[ $scope == "{}" ]]; then
-    echo "No packages affected!" && exit 0
+    echo "No packages affected. Skipping '$@'..." && exit 0
+else
+    echo "Affected packages: $scope"
+    $@ $scope
 fi
-echo $scope
