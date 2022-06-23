e71e8dd9fb52d015695dc32e7401743c48373646
lffloyd <lfmatosmelo@id.uff.br>
2022-06-22 23:44:36 -0300

    test(function): adds error response unit test case

--- packages/function/test/unit/index.spec.ts
+++ packages/function/test/unit/index.spec.ts
@@ -9,7 +9,13 @@ describe('Unit tests for "function" package', () => {
             message: '{\"text\":\"Message\",\"body\":{\"result\":123,\"size\":256}}',
             timestamp: expect.any(Number),
         };
-        const result = await handler({} as APIGatewayProxyEvent);
+        const result = await handler({ something: 'someone' } as unknown as APIGatewayProxyEvent);
         expect(result).toStrictEqual(expected);
+    });
+
+    test('handler call returns expected error response as event object is empty', async () => {
+        const result = await handler({} as APIGatewayProxyEvent);
+        expect(result.statusCode).toBe(500);
+        expect(result.message).toMatch("\"error\":\"Empty event received!\"");
     });
 });
