978244a909da31e8e607f9c92d728e6061b396c4
Luiz Matos <luiz.matos@amedigital.com>
2022-06-24 17:06:43 -0300

    build(scripts): fixes handling of lerna changed output

--- affected.sh
+++ affected.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 changed_packages=$(npx lerna changed)
-scope=$(node -e "console.log('{'+process.argv.slice(1).join(',')+'}')" $changed_packages)
+scope=$(node -e "console.log('{'+process.argv.slice(1).join(',')+',}')" $changed_packages)
 
 if [[ $scope == "{}" ]]; then
     echo "No packages affected. Skipping '$@'..." && exit 0
